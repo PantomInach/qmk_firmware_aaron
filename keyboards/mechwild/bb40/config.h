8447af41425d4b6863f16c52c7b9ba8a31dd113b
Aaron Neugebauer <aaron@ralf-saalmueller.de>
2023-10-04 21:11:04 +0200

    mechwild-bb40: added

---
+++ keyboards/mechwild/bb40/config.h
@@ -0,0 +1,7 @@
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+
+/* Space Cadet rebinds
+ * https://docusaurus.qmk.fm/feature_space_cadet/
+ */
+#define LAPO_KEYS KC_LALT, KC_LSFT, KC_8
+#define RAPC_KEYS KC_RALT, KC_LSFT, KC_9
---
+++ keyboards/mechwild/bb40/rules.mk
@@ -0,0 +1,6 @@
+# Build Options
+#   change yes to no to disable
+#
+
+DEFAULT_FOLDER = mechwild/bb40/f401
+CAPS_WORD_ENABLE = yes
