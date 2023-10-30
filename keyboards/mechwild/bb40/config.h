01d2279dfa502532e859b900b9502995528016d4
Aaron Neugebauer <aaron@ralf-saalmueller.de>
2023-10-30 21:34:21 +0100

    bb40: configurations update

--- keyboards/mechwild/bb40/config.h
+++ keyboards/mechwild/bb40/config.h
@@ -3,5 +3,17 @@
 /* Space Cadet rebinds
  * https://docusaurus.qmk.fm/feature_space_cadet/
  */
-#define LAPO_KEYS KC_LALT, KC_LSFT, KC_8
-#define RAPC_KEYS KC_RALT, KC_LSFT, KC_9
+
+/* For german de layout */
+
+/* Open and close () parenthesis */
+#define LSPO_KEYS KC_LSFT, KC_LSFT, KC_8
+#define RSPO_KEYS KC_LSFT, KC_LSFT, KC_9
+
+/* Open and close [] brackets */
+#define LCPO_KEYS KC_LCTL, KC_RALT, KC_8
+#define RCPO_KEYS KC_LCTL, KC_RALT, KC_9
+
+/* Open and close {} curly brackets */
+#define LAPO_KEYS KC_LCTL, KC_RALT, KC_7
+#define RAPC_KEYS KC_RALT, KC_RALT, KC_0
